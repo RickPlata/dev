e7dc3e85634c2888c1566ef14c4728f29281d748
Ricardo Plata <brownplata@outlook.com>
2025-06-02 13:43:35 -0600

    ok

--- java/spring boot/cursoSpringBoot/src/main/java/com/big/cursoSpringBoot/controllers/CustomerController.java
+++ java/spring boot/cursoSpringBoot/src/main/java/com/big/cursoSpringBoot/controllers/CustomerController.java
@@ -1,15 +1,15 @@
 package com.big.cursoSpringBoot.controllers;
 
 import com.big.cursoSpringBoot.domain.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.propertyeditors.CustomMapEditor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
 
 
@@ -20,12 +20,14 @@ public class CustomerController {
             new Customer(3, "Jorge", "george", "password3")
     ));
 
-    @GetMapping("customers")
+    //@GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public List<Customer> getCustomers(){
         return customers;
     }
 
-    @GetMapping("customers/{username}")
+    //@GetMapping("/{username}")
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
     public Customer getCliente(@PathVariable String username) {
         for(Customer c : customers){
             if(c.getUsername().equalsIgnoreCase(username)){
@@ -34,4 +36,62 @@ public class CustomerController {
         }
         return null;
     }
+
+    //@PostMapping
+    @RequestMapping(method = RequestMethod.POST)
+    public Customer postCliente(@RequestBody Customer customer){
+        customers.add(customer);
+        return customer;
+    }
+
+    //@PutMapping
+    @RequestMapping(method = RequestMethod.PUT)
+    public Customer putCliente(@RequestBody Customer customer){
+        for(Customer c : customers){
+            if(c.getID() == customer.getID()){
+                c.setName(customer.getName());
+                c.setUsername(customer.getUsername());
+                c.setPassword(customer.getPassword());
+
+                System.out.println(c);
+
+                return c;
+            }
+        }
+        return null;
+    }
+
+    //@DeleteMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Customer deleteCliente(@PathVariable int id){
+       for (Customer c: customers){
+           if (c.getID() == id){
+               customers.remove(c);
+
+               return c;
+           }
+       }
+       return null;
+    }
+
+    //@PatchMapping
+    @RequestMapping(method = RequestMethod.PATCH)
+    public Customer patchCliente(@RequestBody Customer customer){
+        for(Customer c : customers){
+            if(c.getID() == customer.getID()){
+                if(customer.getName() != null){
+                    c.setName(customer.getName());
+                }
+                if(customer.getUsername() != null){
+                    c.setUsername(customer.getUsername());
+                }
+                if(customer.getPassword() != null){
+                    c.setPassword(customer.getPassword());
+                }
+            }
+            return c;
+        }
+        return null;
+    }
+
 }
