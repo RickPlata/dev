918ba47aa58571bf8bac3c4f15a7835c6e1f5023
Ricardo Plata <brownplata@outlook.com>
2025-06-18 17:54:24 -0600

    ok

--- java/spring boot/login/src/main/java/com/microservicios/curso/login/component/ApiValues.java
+++ java/spring boot/login/src/main/java/com/microservicios/curso/login/component/ApiValues.java
@@ -2,14 +2,15 @@ package com.microservicios.curso.login.component;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@RefreshScope
 public class ApiValues {
 
     @Value("${session.ttl}")
     private Long sessionTtl;
 
-    private ApiValues() {}
 }
