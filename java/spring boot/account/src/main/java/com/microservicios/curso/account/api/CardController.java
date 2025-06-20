29ce482d53c288f05d470ba8a662f7d54c0983c8
Ricardo Plata <brownplata@outlook.com>
2025-06-20 13:44:12 -0600

    circuit breaker

--- java/spring boot/account/src/main/java/com/microservicios/curso/account/api/CardController.java
+++ java/spring boot/account/src/main/java/com/microservicios/curso/account/api/CardController.java
@@ -1,7 +1,11 @@
 package com.microservicios.curso.account.api;
 
 import com.microservicios.curso.account.service.CardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservicios.curso.account.model.Card;
 
 import java.util.List;
+import java.util.Random;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class CardController {
@@ -18,7 +24,19 @@ public class CardController {
     private CardService cardService;
 
     @GetMapping("/cards/{accountNumber}")
-    public List<Card> getCardsByAccountNumber(@PathVariable("accountNumber") String accountNumber){
-        return cardService.getCardsByAccountNumber(accountNumber);
+    public ResponseEntity<List<Card>> getCardsByAccountNumber(@PathVariable("accountNumber") String accountNumber){
+
+        Random random = new Random();
+
+        int random_number = random.nextInt(99);
+
+        if((random_number % 2) == 0){
+            log.info("cards error");
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+
+
+        return (ResponseEntity<List<Card>>) cardService.getCardsByAccountNumber(accountNumber);
     }
 }
--- java/spring boot/account/src/main/java/com/microservicios/curso/account/client/CardsClient.java
+++ java/spring boot/account/src/main/java/com/microservicios/curso/account/client/CardsClient.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import com.microservicios.curso.account.model.Card;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
 public class CardsClient {
 
@@ -15,12 +18,17 @@ public class CardsClient {
     private RestTemplate restTemplate;
 
     @SuppressWarnings("unchecked")
+    @CircuitBreaker(name = "cards", fallbackMethod = "fallbackGetCardsByAccountNumber")
     public List<Card> getCardsByAccountNumber(String accountNumber){
     
         return restTemplate
                 .getForEntity("http://localhost/api/cards/{accountNumber}", List.class, accountNumber)
                 .getBody();
-  
+    }
+
+    public List<Card> fallbackGetCardsByAccountNumber(Exception ex){
+        log.info("fallback method");
+        return null;
     }
 }
 
