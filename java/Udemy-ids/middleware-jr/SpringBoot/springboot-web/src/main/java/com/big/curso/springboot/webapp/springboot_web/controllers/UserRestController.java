f0920f72319d6deed7ba8954288dfff0ec112595
Ricardo Plata <brownplata@outlook.com>
2025-09-30 18:54:44 -0600

    ok

--- java/Udemy-ids/middleware-jr/SpringBoot/springboot-web/src/main/java/com/big/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ java/Udemy-ids/middleware-jr/SpringBoot/springboot-web/src/main/java/com/big/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,13 +1,17 @@
 package com.big.curso.springboot.webapp.springboot_web.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.big.curso.springboot.webapp.springboot_web.models.User;
+import com.big.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController //tipo de controlador
@@ -15,7 +19,42 @@ import java.util.Map;
 public class UserRestController {
 
     @RequestMapping(path="/details2", method = RequestMethod.GET)// otra forma de definir un metodo rest
-    public Map<String, Object> details(){
+    public UserDto details(){
+
+        UserDto userDto = new UserDto();
+        User user = new User("Ricardo", "Plata");
+        // userDto.setUser(user);// este es el setter de la clase UserDto
+        // userDto.setTitle("Hola Mundo SpringBoot");
+
+        userDto.setName(user.getName());
+        userDto.setLastName(user.getLastName());
+        userDto.setTitle("Hola mundo SpringBoot");
+
+        return userDto;
+
+        //UserDto se es el que utiliza para transaccionar la clase User no se expone
+        
+        
+    }
+
+    @GetMapping("/list")
+    public List<User> list(){
+
+        User user = new User("Ricardo", "Plata");
+        User user2 = new User("Juan", "Perez");
+        User user3 = new User("Jhon", "Doe");
+
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user2);
+        users.add(user3);
+
+        return users;
+    }
+
+
+    @RequestMapping(path="/details-map", method = RequestMethod.GET)// otra forma de definir un metodo rest
+    public Map<String, Object> detailsMap(){
 
         User user = new User("Ricardo", "Plata");
         Map<String, Object> body = new HashMap<>();
---
+++ java/Udemy-ids/middleware-jr/SpringBoot/springboot-web/src/main/java/com/big/curso/springboot/webapp/springboot_web/models/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.big.curso.springboot.webapp.springboot_web.models.dto;
+
+public class UserDto {
+    private String title;
+    private String name;
+    private String lastName;
+
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
