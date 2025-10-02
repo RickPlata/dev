4cd8ed418278cea730720912937cef364d9c856c
Ricardo Plata <brownplata@outlook.com>
2025-10-01 19:06:11 -0600

    ok

--- java/Udemy-ids/middleware-jr/SpringBoot/springboot-web/src/main/java/com/big/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ java/Udemy-ids/middleware-jr/SpringBoot/springboot-web/src/main/java/com/big/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,8 +1,16 @@
 package com.big.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.big.curso.springboot.webapp.springboot_web.models.User;
+
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+
 
 @Controller
 public class UserController {
@@ -10,11 +18,20 @@ public class UserController {
     @GetMapping("/details")
     public String details(Model model){
 
+        User user = new User("Ricardo", "Plata");
         model.addAttribute("title", "Hola Mundo SpringBoot");
-        model.addAttribute("nombre", "BIG");
-        model.addAttribute("lastname", "plata");
+        model.addAttribute("user", user);
 
         
         return "details";
     }
+
+    @GetMapping("/list2")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        return "list";
+    }
+    
 }
--- java/Udemy-ids/middleware-jr/SpringBoot/springboot-web/src/main/java/com/big/curso/springboot/webapp/springboot_web/models/User.java
+++ java/Udemy-ids/middleware-jr/SpringBoot/springboot-web/src/main/java/com/big/curso/springboot/webapp/springboot_web/models/User.java
@@ -4,6 +4,7 @@ public class User {
 
     private String name;
     private String lastName;
+    private String email;
 
     public User(String name, String lastName) {
         this.name = name;
@@ -23,12 +24,20 @@ public class User {
         this.lastName = lastName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
     @Override
     public String toString() {
         return "User [name=" + name + ", lastName=" + lastName + "]";
     }
 
-    
     
 
 }
