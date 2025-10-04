78b4bb5466b9e300f922e17926052efd18a1bc55
Ricardo Plata <brownplata@outlook.com>
2025-10-03 18:40:29 -0600

    ok

--- java/Udemy-ids/middleware-jr/SpringBoot/springboot-crud/src/main/java/com/big/curso/springboot/app/springboot_crud/controllers/ProductController.java
+++ java/Udemy-ids/middleware-jr/SpringBoot/springboot-crud/src/main/java/com/big/curso/springboot/app/springboot_crud/controllers/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.big.curso.springboot.app.springboot_crud.entities.Product;
 import com.big.curso.springboot.app.springboot_crud.services.ProductService;
 
@@ -48,6 +48,17 @@ public class ProductController {
     public ResponseEntity<Product> update(@PathVariable Long id, @PathVariable Product product){
         Product productNew = service.save(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(productNew);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        Product product = new Product();
+        product.setId(id);
+        Optional<Product> productOptional = service.delete(product);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
