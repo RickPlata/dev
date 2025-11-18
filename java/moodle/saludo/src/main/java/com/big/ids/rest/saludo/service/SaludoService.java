748528412eea01fa16d48263185d0fb72ffd09f3
BIG <bigplata@outlook.com>
2025-11-17 22:49:12 -0600

    added saludo project

---
+++ java/moodle/saludo/src/main/java/com/big/ids/rest/saludo/service/SaludoService.java
@@ -0,0 +1,4 @@
+package com.big.ids.rest.saludo.service;
+
+public class SaludoService {
+}
---
+++ java/moodle/saludo/src/test/java/com/big/ids/rest/saludo/SaludoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big.ids.rest.saludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
