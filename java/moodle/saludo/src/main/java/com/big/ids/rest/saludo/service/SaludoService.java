6791b89ec4edaeb6664c8fa1514b6ff1b5719de7
Ricardo Plata <brownplata@outlook.com>
2025-11-18 18:52:51 -0600

    proyecto nivel 1

--- java/moodle/saludo/src/main/java/com/big/ids/rest/saludo/service/SaludoService.java
+++ java/moodle/saludo/src/main/java/com/big/ids/rest/saludo/service/SaludoService.java
@@ -1,4 +1,18 @@
 package com.big.ids.rest.saludo.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SaludoService {
+
+    private String mensaje;
+
+    public SaludoService(@Value("${saludo.mensaje}") String mensaje){
+        this.mensaje = mensaje;
+    }
+
+    public String obtenerSaludo(){
+        return mensaje;
+    }
 }
