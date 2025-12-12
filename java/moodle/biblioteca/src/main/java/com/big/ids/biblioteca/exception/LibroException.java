c0703ce2de3d412f2d30e7da6cca32a09540aabd
BIG <bigplata@outlook.com>
2025-12-11 23:41:49 -0600

    ok

--- java/moodle/biblioteca/src/main/java/com/big/ids/biblioteca/exception/LibroException.java
+++ java/moodle/biblioteca/src/main/java/com/big/ids/biblioteca/exception/LibroException.java
@@ -3,6 +3,6 @@ package com.big.ids.biblioteca.exception;
 public class LibroException extends RuntimeException{
 
     public LibroException(Long id){
-        super("No se encontro el libro con el ID solicitado");
+        super("No se encontro el libro con el ID: " + id);
     }
 }
