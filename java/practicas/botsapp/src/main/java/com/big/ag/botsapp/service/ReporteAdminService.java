02d34bd670d8ba5bf5e0b021cac2f8b25515f93e
Ricardo Plata <brownplata@outlook.com>
2025-12-18 19:17:41 -0600

    reporte admin service

--- java/practicas/botsapp/src/main/java/com/big/ag/botsapp/service/ReporteAdminService.java
+++ java/practicas/botsapp/src/main/java/com/big/ag/botsapp/service/ReporteAdminService.java
@@ -1,7 +1,11 @@
 package com.big.ag.botsapp.service;
 
+import com.big.ag.botsapp.entity.ReporteAdmin;
 import com.big.ag.botsapp.repository.ReporteAdminRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReporteAdminService {
@@ -11,4 +15,43 @@ public class ReporteAdminService {
         public ReporteAdminService(ReporteAdminRepository repository){
             this.repository = repository;
         }
+
+        public List<ReporteAdmin> getAdmin(){
+            return repository.findAll();
+        }
+
+        public Optional<ReporteAdmin> getAdminById(Long id){
+            return repository.findById(id);
+        }
+
+        public ReporteAdmin saveAdmin(ReporteAdmin reporteAdmin){
+            return repository.save(reporteAdmin);
+        }
+
+        public ReporteAdmin updateAdmin(Long id, ReporteAdmin reporteAdmin){
+            return repository.findById(id)
+                    .map(admin -> {
+
+                        admin.setNombre(reporteAdmin.getNombre());
+                        admin.setLab(reporteAdmin.getLab());
+                        admin.setMateria(reporteAdmin.getMateria());
+                        admin.setIncidencia(reporteAdmin.getIncidencia());
+                        admin.setHora(reporteAdmin.getHora());
+                        admin.setFecha(reporteAdmin.getFecha());
+                        admin.setHis(reporteAdmin.getHis());
+
+                        return repository.save(admin); // Hibernate detecta UPDATE
+                    })
+                    .orElseThrow(() -> new RuntimeException("Admin no encontrado con id " + id));
+
+        }
+
+        public void delAdmin(Long id){
+            if(!repository.existsById(id)){
+                throw new RuntimeException("Elemento con el ID: " + id + " no encontrado");
+            }
+            repository.deleteById(id);
+
+        }
+
 }
