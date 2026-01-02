aa07ee8fb8f7a9a5a95cc62c351154735e0623a6
BIG <bigplata@outlook.com>
2026-01-02 00:58:58 -0600

    ok

--- java/practicas/botsapp/src/main/java/com/big/ag/botsapp/controller/ReporteAdminController.java
+++ java/practicas/botsapp/src/main/java/com/big/ag/botsapp/controller/ReporteAdminController.java
@@ -1,11 +1,34 @@
 package com.big.ag.botsapp.controller;
 
+import com.big.ag.botsapp.entity.ReporteAdmin;
+import com.big.ag.botsapp.service.ReporteAdminService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ReporteAdminController {
+
+    private final ReporteAdminService service;
+    public ReporteAdminController(ReporteAdminService service){
+        this.service = service;
+    }
+
+    @GetMapping("/admin")
+    public List<ReporteAdmin> getReportesAdmin)(){
+        return service.getAdmin();
+    }
+
+    @GetMapping("/admin/{id}")
+    public List<ReporteAdmin> getReportesAdminByID)(@PathVariable Long id){
+        return service.getAdminById(id);
+    }
+
+    @
 
 
 }
